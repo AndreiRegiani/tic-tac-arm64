d5072d9d9b682d585965b1c8836031c017b07d3e
Andrei Regiani <andrei.cpp@gmail.com>
2020-06-27 08:35:55 +0200

    WIP

--- src/main.s
+++ src/main.s
@@ -2,7 +2,7 @@
  *  Tic-Tac-ARM64 by @AndreiRegiani
 */
 
-// System calls (linux/include/uapi/asm-generic/unistd.h)
+// System calls [linux/include/uapi/asm-generic/unistd.h]
 .equ    SYS_read,   63
 .equ    SYS_write,  64
 .equ    SYS_exit,   93
@@ -32,23 +32,29 @@ enter_position_str_len = . - enter_position_str
 invalid_str:     .ascii  "Invalid move, try again.\n"
 invalid_str_len = . - invalid_str
 
+win_str:     .ascii  "\n>>> Player has won: "
+win_str_len = . - invalid_str
+
+
 .text
 .globl _start
+
 
 _start:
 	bl      welcome
 
     .main_loop:
-    bl      check_game_over
-    cmp     x0, #4
-    beq     .game_over
     bl      make_move
     bl      draw_board
+    bl      check_game_over
+    cmp     x0, #1
+    beq     .game_over
     bl      switch_player
     b       .main_loop
 
     .game_over:
     bl      exit
+
 
 welcome:
     mov     x0, stdout
@@ -58,16 +64,168 @@ welcome:
 	svc     #0
     ret
 
+
 check_game_over:
-    /*  Returns on x0:
-    0 = in progress
-    1 = player 1 won
-    2 = player 2 won
-    3 = draw
-    4 = game over 
+    ldr     x1, =current_player
+    ldrb    w2, [x1]
+
+    /* Load 3x3 board into the registers:
+
+        w10  w11  w12
+
+        w13  w14  w15
+
+        w16  w17  w18
     */
-    mov     x0, #0
+    ldr     x9, =board
+    ldrb    w10, [x9, #0]
+    ldrb    w11, [x9, #1]
+    ldrb    w12, [x9, #2]
+    ldrb    w13, [x9, #3]
+    ldrb    w14, [x9, #4]
+    ldrb    w15, [x9, #5]
+    ldrb    w16, [x9, #6]
+    ldrb    w17, [x9, #7]
+    ldrb    w18, [x9, #8]
+
+    .win_case_1:
+    /*
+        X X X
+        - - -
+        - - -
+    */
+    cmp     w10, w2  // w2 = current_player, w10 = first element of the board
+    bne     .win_case_2
+    cmp     w11, w2
+    bne     .win_case_2
+    cmp     w12, w2
+    bne     .win_case_2
+    b       player_won
+
+    .win_case_2:
+    /*
+        - - -
+        X X X
+        - - -
+    */
+    cmp     w13, w2
+    bne     .win_case_3
+    cmp     w14, w2
+    bne     .win_case_3
+    cmp     w15, w2
+    bne     .win_case_3
+    b       player_won
+
+    .win_case_3:
+    /*
+        - - -
+        - - -
+        X X X
+    */
+    cmp     w16, w2
+    bne     .win_case_4
+    cmp     w17, w2
+    bne     .win_case_4
+    cmp     w18, w2
+    bne     .win_case_4
+    b       player_won
+
+    .win_case_4:
+    /*
+        X - -
+        X - -
+        X - -
+    */
+    cmp     w10, w2
+    bne     .win_case_5
+    cmp     w13, w2
+    bne     .win_case_5
+    cmp     w16, w2
+    bne     .win_case_5
+    b       player_won
+
+    .win_case_5:
+    /*
+        - X -
+        - X -
+        - X -
+    */
+    cmp     w11, w2
+    bne     .win_case_6
+    cmp     w14, w2
+    bne     .win_case_6
+    cmp     w17, w2
+    bne     .win_case_6
+    b       player_won
+
+    .win_case_6:
+    /*
+        - - X
+        - - X
+        - - X
+    */
+    cmp     w12, w2
+    bne     .win_case_7
+    cmp     w15, w2
+    bne     .win_case_7
+    cmp     w18, w2
+    bne     .win_case_7
+    b       player_won
+
+    .win_case_7:
+    /*
+        X - -
+        - X -
+        - - X
+    */
+    cmp     w10, w2
+    bne     .win_case_8
+    cmp     w14, w2
+    bne     .win_case_8
+    cmp     w18, w2
+    bne     .win_case_8
+    b       player_won
+
+    .win_case_8:
+    /*
+        - - X
+        - X -
+        X - -
+    */
+    cmp     w12, w2
+    bne     .return
+    cmp     w14, w2
+    bne     .return
+    cmp     w16, w2
+    bne     .return
+    b       player_won
+
+    .return:
+    mov     x0, #0 // false: no winners yet
     ret
+
+
+player_won:
+    mov     x0, stdout
+	ldr     x1, =win_str
+	mov     x2, win_str_len
+	mov     x8, SYS_write
+	svc     #0
+
+    mov     x0, stdout
+	ldr     x1, =current_player
+	mov     x2, #1
+	mov     x8, SYS_write
+	svc     #0
+
+    mov     x0, stdout
+	ldr     x1, =new_line
+	mov     x2, #1
+	mov     x8, SYS_write
+	svc     #0
+
+    b       exit
+
 
 draw_board:
     mov     x9, #0 // counter
@@ -108,6 +266,7 @@ draw_board:
     .endloop:
     ret
 
+
 make_move:
     mov     x0, stdout
 	ldr     x1, =enter_position_str
@@ -144,6 +303,7 @@ make_move:
 	svc     #0
     b       make_move
 
+
 switch_player:
     ldr     x11, =current_player
     ldrb    w12, [x11]
@@ -161,6 +321,7 @@ switch_player:
     mov     w12, o_mark
     strb    w12, [x11]
     ret
+
 
 exit:
     mov     x0, #0 // EXIT_SUCCESS
